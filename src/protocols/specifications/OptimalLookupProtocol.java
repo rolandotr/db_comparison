a4bc3e6856f531a2fd1be7f8d3505faea3cf3d4d
rolandotr <rolando.trujillo@gmail.com>
2023-03-16 10:53:28 +0100

    Fixing the optimal protocol

--- src/protocols/specifications/OptimalLookupProtocol.java
+++ src/protocols/specifications/OptimalLookupProtocol.java
@@ -3,82 +3,59 @@ package protocols.specifications;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 
-public class TreeBasedProtocol extends DBProtocol{
+public class OptimalLookupProtocol extends DBProtocol{
 
-	private static final long serialVersionUID = -2997318102832049477L;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private int sizeOfNonces;
 	protected int depth;
-	
-	public static void main(String[] args) {
-		TreeBasedProtocol t = new TreeBasedProtocol();
-		for (int i = 1; i <= 256; i++) {
-			t.setNumberOfRounds(i);
-			System.out.println("n = "+i+" => resistance = "+t.computeMyFar(i));
-		}
-	}
-	
-	public TreeBasedProtocol(int depth, int sizeOfNonces){
+		
+	public OptimalLookupProtocol(int depth, int sizeOfNonces){
 		this.sizeOfNonces = sizeOfNonces;
 		this.depth = depth;
 	}
 
-	public TreeBasedProtocol(int sizeOfNonces){
+	public OptimalLookupProtocol(int sizeOfNonces){
 		this.sizeOfNonces = sizeOfNonces;
 		this.depth = 1;
 	}
 
-	public TreeBasedProtocol(){
+	public OptimalLookupProtocol(){
 		this(1, SIZE_OF_NONCES);
 	}
 	
 	@Override
 	public String getAcronym() {
-		return "tree-based";
+		return "optimal-lookup";
 	}
 
 	@Override
 	public BigDecimal getMafiaFraudProbability() {
-		int trees = n/depth;
-		BigDecimal p = computeMyFar(depth);
-		BigDecimal p1 = p.pow(trees);
-		int remaining = n % depth;
-		BigDecimal p2 = computeMyFar(remaining);
-		if (remaining>1) return p1.multiply(p2);
-		else return p1;
+		return computeMyFar(n);
 	}
 
 	@Override
 	public DBProtocol getInstance() {
-		return new TreeBasedProtocol(depth, SIZE_OF_NONCES);
+		return new OptimalLookupProtocol(depth, SIZE_OF_NONCES);
 	}
 
 	@Override
 	public int getYearOfPublication() {
-		return 2009;
+		return 2023;
 	}
 
 	public BigDecimal computeMyFar(int k) {
 		BigDecimal a = new BigDecimal(0.5d);
 		a = a.pow(k);
-		return a.multiply(new BigDecimal((double)k/2+1));
-	}
-	private BigDecimal distanceFraudUpperBound(int n){
-		BigDecimal half = new BigDecimal(0.5d);
-		BigDecimal p = new BigDecimal(1);
-		for (int i = 1; i <= n; i++){
-			p = p.multiply(half.add(half.pow(i+1)));
-		}
-		BigDecimal four = new BigDecimal(4d);
-		BigDecimal sqrt = half.pow(2*n).add(((four).multiply(half.pow(n))).negate()).add(four.multiply(p));
-		BigDecimal result = half.pow(n);
-		double tt = (result.doubleValue() + Math.sqrt(sqrt.doubleValue()))/2;
-		return new BigDecimal(tt);
+		return a.multiply(new BigDecimal((double)k+1));
 	}
 
 	@Override
 	public BigDecimal getDistanceFraudProbability() {
-		BigDecimal result = distanceFraudUpperBound(depth);
-		return result.pow(n/depth);
+		BigDecimal half = new BigDecimal(0.5d);
+		return half.pow(n);
 	}
 
 	@Override
@@ -94,16 +71,18 @@ public class TreeBasedProtocol extends DBProtocol{
 
 	@Override
 	public int getCryptoCalls() {
-		return 1;
+		int calls = n/depth;
+		if (n % depth == 0) return calls;
+		else return calls+1;
 	}
 
 	@Override
 	public DBProtocol[] getInstances() {
-		TreeBasedProtocol[] result = new TreeBasedProtocol[MAX_N*(32-2+1)];
+		OptimalLookupProtocol[] result = new OptimalLookupProtocol[MAX_N*(32-2+1)];
 		for (int i = 0; i < MAX_N; i++){
 			for (int depth = 2; depth <= 32; depth++) {
 				int index = i*(32-2+1) + depth-2; 
-				result[index] = new TreeBasedProtocol();
+				result[index] = new OptimalLookupProtocol();
 				result[index].setNumberOfRounds(i+1);
 				result[index].depth = depth;
 			}
@@ -113,13 +92,13 @@ public class TreeBasedProtocol extends DBProtocol{
 
 	@Override
 	public String getIdentifier() {
-		return "Tree-{"+n+", "+depth+"}";
+		return "Optimal-{"+n+", "+depth+"}";
 	}
 	
 
 	@Override
 	public boolean lackSecurityProof() {
-		return true;
+		return false;
 	}
 
 	@Override
@@ -152,6 +131,19 @@ public class TreeBasedProtocol extends DBProtocol{
 		return tmp.intValue();
 	}
 	
+	@Override
+	public long getMemory(){
+		long result = getTotalMsgSizeReceived()+
+				SIZE_OF_NONCES*getNumberOfNoncesGenerated()+
+				getBitsGenerated();
+		BigInteger tmp = new BigInteger(2+"");
+		tmp = tmp.pow(depth+1);
+		BigInteger minusTwo = new BigInteger(2+"");
+		minusTwo = minusTwo.negate();
+		tmp = tmp.add(minusTwo);
+		return result + tmp.longValue();
+	}
+
 
 	@Override
 	public int getNumberOfNoncesGenerated() {
